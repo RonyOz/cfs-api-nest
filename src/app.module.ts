eb62aea28a0f4af31e38d26d76f551647df34967
ronyoz <ronyfarido@gmail.com>
2025-10-25 19:44:48 -0500

    add app module imports and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { TypeormConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(TypeormConfig),
+    AuthModule,
+    UsersModule,
+    ProductsModule,
+    OrdersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
