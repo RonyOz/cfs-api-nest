c9fb835bebefb13cf9e9126a453eb6071b1e2708
ronyoz <ronyfarido@gmail.com>
2025-10-25 19:43:40 -0500

    Add TypeORM configuration template

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+export const TypeormConfig: TypeOrmModuleAsyncOptions = {
+  useFactory: () => ({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || 'postgres',
+    database: process.env.DB_NAME || 'cfs',
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: false, // change to true only for dev
+    // TODO: adjust migrations and connection options
+  }),
+};
