2392733b894b8f7f97bfffc28bc5c913e1bd27bb
ronyoz <ronyfarido@gmail.com>
2025-10-27 10:23:17 -0500

    feat: enforce authentication for user management routes

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,6 +3,7 @@ import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @ApiTags('Users')
 @Controller('users')
@@ -10,6 +11,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Auth('admin')
   @ApiOperation({ summary: 'Get all users (admin only)' })
   async findAll() {
     // TODO: enforce admin role and return users
@@ -17,6 +19,7 @@ export class UsersController {
   }
 
   @Post()
+  @Auth('admin')
   @ApiOperation({ summary: 'Create a user (admin only)' })
   async create(@Body() dto: CreateUserDto) {
     // TODO: call usersService.create
@@ -24,18 +27,21 @@ export class UsersController {
   }
 
   @Get(':id')
+  @Auth()
   async findOne(@Param('id') id: string) {
     // TODO: return user by id
     return { user: 'TODO' };
   }
 
   @Put(':id')
+  @Auth()
   async update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
     // TODO: update user
     return { user: 'TODO' };
   }
 
   @Delete(':id')
+  @Auth('admin')
   async remove(@Param('id') id: string) {
     // TODO: delete user
     return { message: 'Deleted' };
