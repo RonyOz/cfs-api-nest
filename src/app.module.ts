afe19358551ae5b3a0254bfeb52cc88bab4a4151
ronyoz <ronyfarido@gmail.com>
2025-11-17 15:10:39 -0500

    fix: disable GraphQL playground and refine context typing in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,28 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { Request, Response } from 'express';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { StorageModule } from './modules/storage/storage.module';
-import { TypeormConfig } from './config/typeorm.config';
 import { SeedModule } from './seeds/seed.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync(TypeormConfig),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
-      playground: true,
-      context: ({ req, res }) => ({ req, res }),
+      playground: false,
+      context: ({ req, res }: { req: Request; res: Response }) => ({
+        req,
+        res,
+      }),
     }),
     AuthModule,
     UsersModule,
@@ -33,4 +34,4 @@ import { SeedModule } from './seeds/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
